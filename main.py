3d7c9b9592ea5389e363da5af8cdeed4433c008d
AndewTs <andew.ts1@gmail.com>
2026-01-27 23:38:36 +0300

    upload main function

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+import arcade
+from window import GameWindow
+from settings import SCREEN_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT
+
+def main():
+    window = GameWindow()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
