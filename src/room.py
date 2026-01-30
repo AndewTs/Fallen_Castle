3a99f97eea12356f48a24c7cf54b7b169ddebe9a
AndewTs <andew.ts1@gmail.com>
2026-01-30 19:15:24 +0300

    Update room.py

--- src/room.py
+++ src/room.py
@@ -5,19 +5,22 @@ class Room:
     def __init__(self, pos, room_type="normal"):
         """
         pos: (x, y) tuple
-        room_type: "start", "normal", "boss"
+        room_type: "start", "normal", "boss", "treasure"
         """
         self.pos = pos
         self.type = room_type
         self.doors = {}  # filled by Floor: {"up": (x,y), ...}
-        self.enemy_spawns = []  # list of dicts {"type":"enemy"/"boss","x":int,"y":int,"hp":int}
-        self.walls = []  # optional positions of obstacles
-        self.forbidden_zones = []
+        self.enemy_spawns = []  # list of dicts {"type":"enemy","x":int,"y":int}
+        self.walls = []  # optional positions of obstacles (not sprites)
+        self.forbidden_zones = []  # list of (x,y,radius)
+        self.treasure_unlocked = False  # for treasure rooms
 
         # enemy count by type
         if self.type == "start":
             cnt = 0
         elif self.type == "boss":
+            cnt = 0
+        elif self.type == "treasure":
             cnt = 0
         else:
             cnt = random.randint(2, 4)
@@ -33,3 +36,4 @@ class Room:
 
     def add_forbidden_zone(self, x, y, radius):
         self.forbidden_zones.append((x, y, radius))
+
