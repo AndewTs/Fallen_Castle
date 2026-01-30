b625f548df2455305038ef982d273e9602723706
AndewTs <andew.ts1@gmail.com>
2026-01-30 19:11:47 +0300

    Directions now are located in settings

--- src/dungeon.py
+++ src/dungeon.py
@@ -1,20 +1,6 @@
-# dungeon.py
 from room import Room
-from settings import ROOM_GRID_SIZE
-
-DIRECTIONS = {
-    "up": (0, 1),
-    "down": (0, -1),
-    "left": (-1, 0),
-    "right": (1, 0)
-}
-
-OPPOSITE = {
-    "up": "down",
-    "down": "up",
-    "left": "right",
-    "right": "left"
-}
+import random
+from settings import ROOM_GRID_SIZE, DIRECTIONS, SCREEN_HEIGHT, SCREEN_WIDTH
 
 
 class Floor:
@@ -24,6 +10,7 @@ class Floor:
         self.start_pos = (0, 0)
         self.boss_pos = (size - 1, size - 1)
         self.current_pos = self.start_pos
+        self.treasure_pos = None
         self.generate()
 
     def generate(self):
@@ -39,6 +26,14 @@ class Floor:
                     r = Room(pos, "normal")
                 self.rooms[pos] = r
 
+        # pick one random normal room to be treasure (not start, not boss)
+        normal_positions = [p for p, r in self.rooms.items() if r.type == "normal"]
+        if normal_positions:
+            tp = random.choice(normal_positions)
+            self.rooms[tp].type = "treasure"
+            self.rooms[tp].treasure_unlocked = False
+            self.treasure_pos = tp
+
         # connect doors (4-neighbors)
         for (x, y), room in list(self.rooms.items()):
             doors = {}
@@ -48,22 +43,21 @@ class Floor:
                     doors[name] = npos
             room.set_doors(doors)
 
-            SCREEN_W = 1280
-            SCREEN_H = 720
-
-            DOOR_MARGIN = 200
-
-            if "up" in doors:
+        # Add forbidden zones for doors & start
+        SCREEN_W = SCREEN_WIDTH
+        SCREEN_H = SCREEN_HEIGHT
+        DOOR_MARGIN = 140
+        for (x, y), room in self.rooms.items():
+            if "up" in room.doors:
                 room.add_forbidden_zone(SCREEN_W // 2, SCREEN_H - 80, DOOR_MARGIN)
-            if "down" in doors:
+            if "down" in room.doors:
                 room.add_forbidden_zone(SCREEN_W // 2, 80, DOOR_MARGIN)
-            if "left" in doors:
+            if "left" in room.doors:
                 room.add_forbidden_zone(80, SCREEN_H // 2, DOOR_MARGIN)
-            if "right" in doors:
+            if "right" in room.doors:
                 room.add_forbidden_zone(SCREEN_W - 80, SCREEN_H // 2, DOOR_MARGIN)
             if room.type == "start":
                 room.add_forbidden_zone(SCREEN_W // 2, 190, 200)
-
 
     def get_current_room(self):
         return self.rooms[self.current_pos]
@@ -73,3 +67,4 @@ class Floor:
             self.current_pos = pos
             return True
         return False
+
